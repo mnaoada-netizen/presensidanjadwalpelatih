be8d2d06ab91960ec45aa080b069e0d43871bad0
mnaoada-netizen <mnaoada@gmail.com>
2026-04-21 08:07:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# presensidanjadwalpelatih
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/mnaoada-netizen/presensidanjadwalpelatih)
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Example />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
